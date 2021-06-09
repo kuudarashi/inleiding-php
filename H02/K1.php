a65f4f9b9c8772200187064d7a8fcc889ec49b17
kuudarashi <creepas21@gmail.com>
2021-06-09 20:05:26 +0200

    tot 3 af

---
+++ H02/K1.php
@@ -0,0 +1,6 @@
+<?php
+ $a = "ik heb niet gefaald. ";
+ $b = "die niet werken.";
+ $c = 10000;
+
+ echo $a."ik heb alleen ".$c." manieren gevonden ".$b."\"";
---
+++ H02/K2.php
@@ -0,0 +1,6 @@
+<?php
+$a = "die geen fout";
+$b = "en";
+$c = "niet";
+
+echo "Mensen ".$a.$b." maken, "."werken ".$c;
